579d7f79b4f635853287ec2ba37f9ea289a55779
Ploy <642021147@tsu.ac.th>
2023-12-15 15:48:24 +0700

    ploouy

---
+++ android/app/src/main/kotlin/com/example/lab04_147/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lab04_147
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/homepage.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String foodRadio = "";
+
+  bool starEgg = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("เมณู อาหารป่า"),
+        ),
+        body: Column(
+          children: [
+            RadioManulist("ปลากัดต้มแซ่บ", "250"),
+            RadioManulist("ยำยุงลายป่า", "220"),
+            RadioManulist("ข้าวผัดงูหลาม", "200"),
+            RadioManulist("ข้าวผัดงูหลาม", "200"),
+            Divider(),
+            CheckboxListTile(
+                controlAffinity: ListTileControlAffinity.leading,
+                title: Text("ไข่นกกระทาดาว"),
+                subtitle: Text("0.5"),
+                value: starEgg,
+                onChanged: (value) {
+                  setState(() {
+                    starEgg = value!;
+                  });
+                })
+          ],
+        ));
+  }
+
+  RadioListTile<String> RadioManulist(String title, String subtitle) {
+    return RadioListTile(
+      title: Text(title),
+      subtitle: Text("189"),
+      value: subtitle,
+      groupValue: foodRadio,
+      onChanged: (value) {
+        setState(() {
+          foodRadio = value!;
+        });
+      },
+    );
+  }
+}
